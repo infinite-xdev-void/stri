4dc30373844fdbb5ddf6bb53281689946dd4290b
marwan <marwan.hilal.dev@gmail.com>
2025-07-21 13:17:12 +0300

    reuse 'v' name instead of redefine a new one for identical expressions

--- stri_proc/src/parts.rs
+++ stri_proc/src/parts.rs
@@ -2,7 +2,7 @@
 //
 //
 
-use std::marker::PhantomData;
+use std::{collections::HashMap, marker::PhantomData};
 
 //
 //
@@ -13,6 +13,7 @@ use std::marker::PhantomData;
 //
 
 use quote::{format_ident, quote};
+use syn::Ident;
 
 //
 //
@@ -33,12 +34,13 @@ use crate::{Format, Ts2};
 //
 
 pub struct Parts<F: Format> {
-    s_lens: usize,
-    vars: Ts2,
-    v_lens: Ts2,
-    v_count: usize,
-    pushs: Ts2,
-    f: PhantomData<F>,
+   s_lens: usize,
+   var_idens: HashMap<Vec<u8>, Ident>,
+   vars: Ts2,
+   v_lens: Ts2,
+   v_count: usize,
+   pushs: Ts2,
+   f: PhantomData<F>,
 }
 
 //
@@ -46,129 +48,157 @@ pub struct Parts<F: Format> {
 //
 
 impl<F: Format> Parts<F> {
-    //
-    //
-    //
-    #[inline(always)]
-    pub fn new() -> Self {
-        Self {
-            s_lens: 0,
-            vars: Ts2::new(),
-            v_lens: Ts2::new(),
-            v_count: 0,
-            pushs: Ts2::new(),
-            f: PhantomData,
-        }
-    }
+   //
+   //
+   //
+   #[inline(always)]
+   pub fn new() -> Self {
+      Self {
+         s_lens: 0,
+         var_idens: HashMap::new(),
+         vars: Ts2::new(),
+         v_lens: Ts2::new(),
+         v_count: 0,
+         pushs: Ts2::new(),
+         f: PhantomData,
+      }
+   }
 
-    //
-    //
-    //
-    //
-    //
-    //
-    //
+   //
+   //
+   //
+   //
+   //
+   //
+   //
 
-    #[inline(always)]
-    pub fn add_expr(&mut self, expr: &[u8]) {
-        let expr = unsafe { core::str::from_utf8_unchecked(expr) };
+   #[inline(always)]
+   pub fn add_expr(&mut self, expr: &[u8]) {
+      //
+      //
+      //
 
-        let ident = format_ident!("v{}", self.v_count);
+      match self.var_idens.get(expr) {
+         Some(ident) => {
+            extend(
+               &mut self.v_lens,
+               quote! {+ ::stri::Interpolator::len(&#ident)},
+            );
 
-        let var_def = F::var_def(&ident, expr.trim());
+            extend(
+               &mut self.pushs,
+               quote! {::stri::Interpolator::interpolate(&#ident, &mut s);},
+            );
+         }
 
-        extend(&mut self.vars, var_def);
-        extend(
-            &mut self.v_lens,
-            quote! {+ ::stri::Interpolator::len(&#ident)},
-        );
-        extend(
-            &mut self.pushs,
-            quote! {::stri::Interpolator::interpolate(&#ident, &mut s);},
-        );
-        self.v_count += 1;
-    }
+         //
+         //
+         //
+         //
+         //
+         None => {
+            let ident = format_ident!("v{}", self.v_count);
+            self.var_idens.insert(expr.to_vec(), ident.clone());
 
-    //
-    //
-    //
-    //
-    //
-    //
-    //
+            let expr = unsafe { core::str::from_utf8_unchecked(expr) };
 
-    #[inline(always)]
-    pub fn push_str(&mut self, s: &[u8]) {
-        if s.len() == 0 {
-            return;
-        };
+            let var_def = F::var_def(&ident, expr.trim());
 
-        let s = unsafe { String::from_utf8_unchecked(s.to_vec()) };
-        self.s_lens += s.len();
-        extend(&mut self.pushs, quote! {s.push_str(#s);});
-    }
+            extend(&mut self.vars, var_def);
+            extend(
+               &mut self.v_lens,
+               quote! {+ ::stri::Interpolator::len(&#ident)},
+            );
+            extend(
+               &mut self.pushs,
+               quote! {::stri::Interpolator::interpolate(&#ident, &mut s);},
+            );
+            self.v_count += 1;
+         }
+      };
+   }
 
-    //
-    //
-    //
-    //
-    //
-    //
-    //
+   //
+   //
+   //
+   //
+   //
+   //
+   //
 
-    #[inline(always)]
-    pub fn build(self) -> Ts2 {
-        let Self {
-            s_lens,
-            mut vars,
-            v_lens,
-            v_count,
-            pushs,
-            f: _,
-        } = self;
+   #[inline(always)]
+   pub fn push_str(&mut self, s: &[u8]) {
+      if s.len() == 0 {
+         return;
+      };
 
-        //
-        //
-        //
+      let s = unsafe { String::from_utf8_unchecked(s.to_vec()) };
+      self.s_lens += s.len();
+      extend(&mut self.pushs, quote! {s.push_str(#s);});
+   }
 
-        if s_lens == 0 && v_count == 0 {
-            return Ts2::new();
-        };
+   //
+   //
+   //
+   //
+   //
+   //
+   //
 
-        //
-        //
-        //
-        //
-        //
-        //
-        //
+   #[inline(always)]
+   pub fn build(self) -> Ts2 {
+      let Self {
+         s_lens,
+         var_idens: _,
+         mut vars,
+         v_lens,
+         v_count,
+         pushs,
+         f: _,
+      } = self;
 
-        extend(
-            &mut vars,
-            quote! {let mut s = String::with_capacity(#s_lens #v_lens);},
-        );
+      //
+      //
+      //
 
-        //
-        //
-        //
-        //
-        //
-        //
-        //
+      if s_lens == 0 && v_count == 0 {
+         return Ts2::new();
+      };
 
-        quote! {
-          {#vars
+      //
+      //
+      //
+      //
+      //
+      //
+      //
 
-          #pushs
+      extend(
+         &mut vars,
+         quote! {let mut s = String::with_capacity(#s_lens #v_lens);},
+      );
 
-          s}
-        }
-    }
+      //
+      //
+      //
+      //
+      //
+      //
+      //
+
+      quote! {
+        {#vars
+
+        #pushs
+
+        s}
+      }
+   }
 }
 
 #[inline(always)]
 pub fn extend(target: &mut Ts2, source: Ts2) {
-    target.extend(core::iter::once(source));
+   target.extend(core::iter::once(source));
 }
 
 /*
