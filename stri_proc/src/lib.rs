b33bb232aceb618d9df8e363c0156e44eeb703c9
marwan <marwan.hilal.dev@gmail.com>
2025-07-23 12:42:21 +0300

    add 'dir!' macro

--- stri_proc/src/lib.rs
+++ stri_proc/src/lib.rs
@@ -2,7 +2,7 @@
 //
 //
 
-use std::fmt::Display;
+use std::{fmt::Display, fs::ReadDir, os::windows::fs::FileTypeExt};
 
 //
 //
@@ -43,7 +43,7 @@ use syn::{LitStr, parse_macro_input, spanned::Spanned};
 //
 
 mod format;
-use format::Format;
+use format::{Format, Sql, Str};
 
 //
 //
@@ -56,8 +56,6 @@ use format::Format;
 mod parts;
 use parts::Parts;
 
-use crate::format::{Sql, Str};
-
 //
 //
 //
@@ -68,7 +66,7 @@ use crate::format::{Sql, Str};
 
 #[proc_macro]
 pub fn si(i: Ts) -> Ts {
-    From::from(build_str::<Str>(parse_macro_input!(i as LitStr).value()))
+   From::from(build_str::<Str>(parse_macro_input!(i as LitStr).value()))
 }
 
 //
@@ -77,7 +75,7 @@ pub fn si(i: Ts) -> Ts {
 
 #[proc_macro]
 pub fn sql(i: Ts) -> Ts {
-    From::from(build_str::<Sql>(parse_macro_input!(i as LitStr).value()))
+   From::from(build_str::<Sql>(parse_macro_input!(i as LitStr).value()))
 }
 
 //
@@ -86,28 +84,98 @@ pub fn sql(i: Ts) -> Ts {
 
 #[proc_macro]
 pub fn file(f: Ts) -> Ts {
-    let manifest_dir = std::env::var("CARGO_MANIFEST_DIR")
-        .expect("CARGO_MANIFEST_DIR not set. This macro should be compiled by Cargo.");
+   let manifest_dir = std::env::var("CARGO_MANIFEST_DIR").expect(
+      "CARGO_MANIFEST_DIR not set. This macro should be compiled by Cargo.",
+   );
 
-    //
-    //
-    //
+   //
+   //
+   //
 
-    let file_name = parse_macro_input!(f as LitStr).value();
+   let file_name = parse_macro_input!(f as LitStr).value();
 
-    let path = std::path::PathBuf::from(manifest_dir).join(&file_name);
+   let path = std::path::PathBuf::from(manifest_dir).join(&file_name);
 
-    //
-    //
-    //
+   //
+   //
+   //
 
-    let file = std::fs::read_to_string(&path).unwrap();
+   let file = std::fs::read_to_string(&path).unwrap();
 
-    if file_name.ends_with(".sql") {
-        From::from(build_str::<Sql>(file))
-    } else {
-        From::from(build_str::<Str>(file))
-    }
+   if file_name.ends_with(".sql") {
+      From::from(build_str::<Sql>(file))
+   } else {
+      From::from(build_str::<Str>(file))
+   }
+}
+
+//
+//
+//
+//
+//
+//
+//
+#[proc_macro]
+pub fn dir(d: Ts) -> Ts {
+   let manifest_dir = std::env::var("CARGO_MANIFEST_DIR").expect(
+      "CARGO_MANIFEST_DIR not set. This macro should be compiled by Cargo.",
+   );
+
+   let dir_name = parse_macro_input!(d as LitStr).value();
+
+   let path = std::path::PathBuf::from(manifest_dir).join(&dir_name);
+
+   //
+   //
+   //
+
+   let (val, is_sql) =
+      concat_first_lvl_files_in_dir(std::fs::read_dir(path).unwrap());
+
+   //
+
+   From::from(match is_sql {
+      true => build_str::<Sql>(val),
+      false => build_str::<Str>(val),
+   })
+}
+
+//
+//
+//
+
+#[inline(always)]
+fn concat_first_lvl_files_in_dir(dir: ReadDir) -> (String, bool) {
+   let mut contents = Vec::new();
+   let mut all_sql = true;
+
+   let mut file_type;
+
+   for entity in dir {
+      let entity = entity.unwrap();
+      file_type = entity.file_type().unwrap();
+      if file_type.is_file() || file_type.is_symlink_file() {
+         let path = entity.path();
+
+         if let Some(e) = path.extension() {
+            if e != "sql" {
+               all_sql = false;
+            }
+         } else {
+            all_sql = false;
+         }
+
+         contents
+            .push(std::fs::read_to_string(path).unwrap().trim().to_owned());
+      };
+   }
+
+   if contents.len() == 0 {
+      panic!("there are no files in the first level of the directory.")
+   }
+
+   (contents.join("\n"), all_sql)
 }
 
 //
@@ -119,126 +187,126 @@ pub fn file(f: Ts) -> Ts {
 //
 
 fn build_str<F: Format>(val: String) -> Ts2 {
-    //
-    //
-    //
+   //
+   //
+   //
 
-    let mut parts = Parts::<F>::new();
+   let mut parts = Parts::<F>::new();
 
-    let bs = val.as_bytes();
+   let bs = val.as_bytes();
 
-    let mut s = Vec::new();
-    let mut e = Vec::new();
+   let mut s = Vec::new();
+   let mut e = Vec::new();
 
-    //
-    //
-    //
+   //
+   //
+   //
 
-    let mut i = 0;
+   let mut i = 0;
 
-    'outer: while i < bs.len() {
-        //
-        //
-        //
+   'outer: while i < bs.len() {
+      //
+      //
+      //
 
-        match bs[i] {
+      match bs[i] {
+         //
+         //
+         //
+         b'{' => {
+            let Some(n) = bs.get(i + 1) else {
+               return compile_err(
+                  &i,
+                  "invalid string: expected expression but string was terminated if you intended to add `{`, you can escape it using `{{`",
+               );
+            };
+
             //
             //
             //
-            b'{' => {
-                let Some(n) = bs.get(i + 1) else {
-                    return compile_err(
-                        &i,
-                        "invalid string: expected expression but string was terminated if you intended to add `{`, you can escape it using `{{`",
-                    );
-                };
 
-                //
-                //
-                //
+            if n.eq(&b'{') {
+               s.push(b'{');
+               i += 2;
+               continue 'outer;
+            };
 
-                if n.eq(&b'{') {
-                    s.push(b'{');
-                    i += 2;
-                    continue 'outer;
-                };
+            //
+            //
+            //
+            //
+            //
+            //
+            //
 
-                //
-                //
-                //
-                //
-                //
-                //
-                //
+            parts.push_str(&s);
+            s.clear();
 
-                parts.push_str(&s);
-                s.clear();
+            //
+            //
+            //
 
-                //
-                //
-                //
+            e.push(*n);
+            i += 2;
 
-                e.push(*n);
-                i += 2;
+            //
+            //
+            //
 
-                //
-                //
-                //
+            let mut depth = 0;
 
-                let mut depth = 0;
+            //
+            //
+            //
 
-                //
-                //
-                //
+            while let Some(b) = bs.get(i) {
+               if b.eq(&b'{') {
+                  depth += 1;
+               };
 
-                while let Some(b) = bs.get(i) {
-                    if b.eq(&b'{') {
-                        depth += 1;
-                    };
+               if b.eq(&b'}') {
+                  if let Some(n) = bs.get(i + 1) {
+                     if n.eq(&b'}') {
+                        e.push(b'}');
+                        e.push(b'}');
+                        i += 2;
+                        continue;
+                     };
+                  };
 
-                    if b.eq(&b'}') {
-                        if let Some(n) = bs.get(i + 1) {
-                            if n.eq(&b'}') {
-                                e.push(b'}');
-                                e.push(b'}');
-                                i += 2;
-                                continue;
-                            };
-                        };
+                  if depth == 0 {
+                     parts.add_expr(&e);
+                     e.clear();
+                     i += 1;
+                     continue 'outer;
+                  } else {
+                     depth -= 1;
+                  }
+               };
 
-                        if depth == 0 {
-                            parts.add_expr(&e);
-                            e.clear();
-                            i += 1;
-                            continue 'outer;
-                        } else {
-                            depth -= 1;
-                        }
-                    };
-
-                    e.push(*b);
-                    i += 1;
-                }
-
-                //
-                //
-                //
-
-                return compile_err(
-                    &val,
-                    "invalid string: expected `}` but string was terminated",
-                );
+               e.push(*b);
+               i += 1;
             }
 
-            b @ _ => s.push(b),
-        }
+            //
+            //
+            //
 
-        i += 1;
-    }
+            return compile_err(
+               &val,
+               "invalid string: expected `}` but string was terminated",
+            );
+         }
 
-    parts.push_str(&s);
+         b @ _ => s.push(b),
+      }
 
-    parts.build()
+      i += 1;
+   }
+
+   parts.push_str(&s);
+
+   parts.build()
 }
 
 //
@@ -288,5 +356,5 @@ fn get_expr<S: Spanned>(s: &S, iter: &mut Iter<'_, u8>) -> Result<Ts2, Ts2> {
 
 #[inline(always)]
 fn compile_err<S: Spanned, M: Display>(s: &S, m: M) -> Ts2 {
-    syn::Error::new(s.span(), m).to_compile_error()
+   syn::Error::new(s.span(), m).to_compile_error()
 }
